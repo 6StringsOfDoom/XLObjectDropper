a95b3c983889a28b8be81ad0668155fcd085f8e9
Bryan Taylor <mcbtay@gmail.com>
2021-06-04 22:11:50 -0400

    adding RigidbodyReplayInfo

---
+++ XLObjectDropper/Utilities/RigidbodyReplayInfo.cs
@@ -0,0 +1,20 @@
+﻿using SkaterXL.Data;
+using UnityEngine;
+
+namespace XLObjectDropper.Utilities
+{
+	public class RigidbodyReplayInfo
+	{
+		public TransformInfo transformInfo;
+		public float time;
+
+		public bool addedToAnimation;
+
+		public RigidbodyReplayInfo(Transform transform, float time)
+		{
+			transformInfo = new TransformInfo(transform);
+			this.time = time;
+			this.addedToAnimation = false;
+		}
+	}
+}
